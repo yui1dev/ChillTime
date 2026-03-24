45d5f80eaeca0291f7348a55934960f4f7b86882
Fotimaizahro <fotimaqoldasheva149@gmail.com>
2026-03-24 15:39:18 +0500

    second

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
+// var swiper = new Swiper(".mySwiper", {
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+
 var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    }
+  }
+});
